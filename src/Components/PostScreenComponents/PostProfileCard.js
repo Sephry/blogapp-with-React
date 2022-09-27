f9e4802605d899d5bdb622669dbd62a4769c644f
Sephry <ismail.kurtf@gmail.com>
2022-09-27 12:42:28 +0300

    post screen profile card update

---
+++ src/Components/PostScreenComponents/PostProfileCard.js
@@ -0,0 +1,81 @@
+
+
+const PostProfileCard = () => {
+
+    return (
+
+        <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm p-4 border-t border-b md:border md:rounded">
+            <img src="https://source.unsplash.com/150x150/?portrait?3" alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
+            <div className="space-y-4 text-center divide-y divide-gray-700">
+                <div className="my-2 space-y-1">
+                    <h2 className="text-xl font-semibold sm:text-2xl">Leroy Jenkins</h2>
+                    <p className="px-5 text-xs sm:text-base dark:text-gray-400">Full-stack developer</p>
+                </div>
+                <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
+                    <a rel="noopener noreferrer" href="#" aria-label="Email" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
+                        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                            <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
+                        </svg>
+                    </a>
+                    ismail.kurtf@gmail.com
+                    <i className="bx bx-user-plus ml-2"></i>
+                </button>
+                <div className="flex justify-center pt-2 space-x-4 align-center">
+                    <a rel="noopener noreferrer" href="#" aria-label="GitHub" className="p-2 rounded-md dark:text-gray-700 hover:dark:text-violet-400">
+                        <svg viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                        </svg>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" aria-label="Linkedin" className="p-2 rounded-md dark:text-gray-700 hover:dark:text-violet-400">
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />
+                        </svg>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" aria-label="Instagram" className="p-2 rounded-md dark:text-gray-700 hover:dark:text-violet-400">
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                        </svg>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" aria-label="Twitter" className="p-2 rounded-md dark:text-gray-700 hover:dark:text-violet-400">
+                        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
+                            <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+                        </svg>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+        /*
+                <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
+                    <div className="p-4 border-t border-b md:border md:rounded">
+                        <div className="flex py-2">
+                            <img
+                                src="https://randomuser.me/api/portraits/men/97.jpg"
+                                className="h-10 w-10 rounded-full mr-2 object-cover"
+                            />
+                            <div>
+                                <p className="font-semibold text-gray-700 text-sm">Mike Sullivan</p>
+                                <p className="font-semibold text-gray-600 text-xs">Editor</p>
+                            </div>
+                        </div>
+                        <p className="text-gray-700 py-3">
+                            Mike writes about technology Yourself required no at thoughts
+                            delicate landlord it be. Branched dashwood do is whatever it.
+                        </p>
+                        <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
+                            ismail.kurtf@gmail.com
+                            <i className="bx bx-user-plus ml-2"></i>
+                        </button>
+        
+                        <button className="px-2 py-1 my-2 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
+        
+        
+                            <i className="bx bx-user-plus ml-2"></i>
+                        </button>   
+                    </div>
+                </div>
+        */
+    );
+}
+
+export default PostProfileCard;
--- src/Pages/PostScreen.js
+++ src/Pages/PostScreen.js
@@ -1,3 +1,4 @@
+import PostProfileCard from "../Components/PostScreenComponents/PostProfileCard";
 import PostTitle from "../Components/PostTitle";
 
 export default function PostScreen(params) {
@@ -110,28 +111,8 @@ export default function PostScreen(params) {
           </p>
         </div>
 
-        <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
-          <div className="p-4 border-t border-b md:border md:rounded">
-            <div className="flex py-2">
-              <img
-                src="https://randomuser.me/api/portraits/men/97.jpg"
-                className="h-10 w-10 rounded-full mr-2 object-cover"
-              />
-              <div>
-                <p className="font-semibold text-gray-700 text-sm">Mike Sullivan</p>
-                <p className="font-semibold text-gray-600 text-xs">Editor</p>
-              </div>
-            </div>
-            <p className="text-gray-700 py-3">
-              Mike writes about technology Yourself required no at thoughts
-              delicate landlord it be. Branched dashwood do is whatever it.
-            </p>
-            <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
-              Follow
-              <i className="bx bx-user-plus ml-2"></i>
-            </button>
-          </div>
-        </div>
+        <PostProfileCard />
+
       </div>
     </main>
   );
